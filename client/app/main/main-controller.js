dc2eed5b67265e6b5bf85665b5f53ac020d1cdd4
Frederic Nell <fnell@csc.com>
2016-05-24 14:14:40 +0200

    add a link

--- client/app/main/main-controller.js
+++ client/app/main/main-controller.js
@@ -2,7 +2,7 @@ angular.module('app')
   .controller('MainCtrl', ['$scope', function ($scope) {
     // here we define the items to be repeated in the template
     $scope.list1 = [
-      {label: '<a href="https://docs.openshift.org/latest/dev_guide/managing_images.html#importing-tag-and-image-metadata"> openshift image stream </a>'},
+      {label: '<a href=\'https://docs.openshift.org/latest/dev_guide/managing_images.html#importing-tag-and-image-metadata\'> openshift image stream </a>'},
       {label: 'two'},
       {label: 'three'}
     ];
